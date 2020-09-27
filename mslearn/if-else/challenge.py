37de4b5554ac8f16e53c45971ba932c8b27c108c
Andre Sasongko <sasongko@gmbcgroup.com>
2020-09-27 15:46:03 +0200

    changes on 2020-09-27

---
+++ mslearn/if-else/challenge.py
@@ -0,0 +1,8 @@
+answer = input("Would you like to continue? ")
+if answer == "yes" or answer == "y":
+    print("Continue ...")
+    print("Complete!")
+elif answer == "no" or answer == "n":
+    print("Exiting")
+else:
+    print("Please try again and respond with yes or no.")
