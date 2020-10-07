f9e69f7b5445504e59eb5a42b080b483f2e1aeb1
Andre Sasongko <as@ansaworks.com>
2020-10-07 21:42:18 +0200

    Changes on 2020-10-07

--- mslearn/python-format-strings/exercise2.py
+++ mslearn/python-format-strings/exercise2.py
@@ -1,42 +1,48 @@
-#message = str.capitalize('first message')
-#print(message)
-#message = 'second message'.capitalize()
-#print (message)
-#message = 'third message'
-#print(message.capitalize())
+message = str.capitalize('first message')
+print(message)
+message = 'second message'.capitalize()
+print (message)
+message = 'third message'
+print(message.capitalize())
+print('=====================\n')
 
-#message = 'hello world'
-#print(message.lower())
-#print(message.upper())
-#message = message.title()
-#print(message)
-#print(message.swapcase())
+message = 'hello world'
+print(message.lower())
+print(message.upper())
+message = message.title()
+print(message)
+print(message.swapcase())
+print('=====================\n')
 
-#location = 'Mississippi'
-#print(location.count('s'))
+location = 'Mississippi'
+print(location.count('s'))
+print('=====================\n')
 
-#print(len('how many letters in this string?'))
+print(len('how many letters in this string?'))
+message = 'racecar'
+print(message.startswith('r'))
+print(message.startswith('a'))
+print(message.startswith('ra'))
+print(message.endswith('r'))
+print(message.endswith('a'))
+print(message.endswith('ar'))
+print('=====================\n')
 
-#message = 'racecar'
-#print(message.startswith('r'))
-#print(message.startswith('a'))
-#print(message.startswith('ra'))
-#print(message.endswith('r'))
-#print(message.endswith('a'))
-#print(message.endswith('ar'))
+message = 'The quick brown fox jumps over the lazy dog'
+print(message.find('q'))
+print(message.find('t'))
+print(message.find('T'))
+print('=====================\n')
 
-#message = 'The quick brown fox jumps over the lazy dog'
-#print(message.find('q'))
-#print(message.find('t'))
-#print(message.find('T'))
+message = '    middle     '
+print(message.lstrip() + '.')
+print(message.rstrip() + '.')
+print(message.strip() + '.')
+print('=====================\n')
 
-#message = '    middle     '
-#print(message.lstrip() + '.')
-#print(message.rstrip() + '.')
-#print(message.strip() + '.')
-
-#message = 'brevity is the essence of wit'
-#print(message.replace('essence','soul'))
+message = 'brevity is the essence of wit'
+print(message.replace('essence','soul'))
+print('=====================\n')
 
 message = 'howdy'
 print(message.rjust(20))
